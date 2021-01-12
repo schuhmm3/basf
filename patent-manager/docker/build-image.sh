49c76236337b25c4a3bcba9992a25e2d899362be
Roberto Gomez <rogomdi@gmail.com>
2021-01-12 22:41:04 +0100

    Adds first version of CI and CD pipelines

--- patent-manager/docker/build-image.sh
+++ patent-manager/docker/build-image.sh
@@ -2,6 +2,6 @@
 if [[ -z ${VERSION} ]]; then
   VERSION="latest"
 fi
-cp ../build/libs/*.jar app.jar
+cp ../build/libs/*-executable.jar app.jar
 docker build . -t "${REGISTRY}"patent-manager:${VERSION} --no-cache
 rm app.jar
