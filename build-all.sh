677516ebf2ec6cd79ad5454128a99ae74b29bdeb
Roberto Gomez <rogomdi@gmail.com>
2021-01-13 22:21:12 +0100

    Fix publish chart

--- build-all.sh
+++ build-all.sh
@@ -2,20 +2,25 @@
 if [[ -z ${CHART_VERSION} ]]; then
   CHART_VERSION="1.0.0"
 fi
-home=$PWD
-echo "Building NLP Processor"
-cd nlp-processor
-./gradlew clean build -PskipTests
-cd docker
-bash build-image.sh
-cd $home
+if [[ -z ${BUILD_IMAGES} ]]; then
+  BUILD_IMAGES=false
+fi
+if [ "$BUILD_IMAGES" = true ] ; then
+  home=$PWD
+  echo "Building NLP Processor"
+  cd nlp-processor || exit
+  ./gradlew clean build -PskipTests
+  cd docker || exit
+  bash build-image.sh
+  cd $home || exit
 
-echo "Building Patent manager"
-cd patent-manager
-./gradlew clean build -PskipTests
-cd docker
-bash build-image.sh
-cd $home
+  echo "Building Patent manager"
+  cd patent-manager || exit
+  ./gradlew clean build -PskipTests
+  cd docker || exit
+  bash build-image.sh
+  cd $home || exit
+fi
 
 echo "Building chart"
 cp docker-compose.yml basf-test.yml
